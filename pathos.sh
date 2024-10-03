59584c69c23c1ca8e3c8d7f51af3f3e0c71384ca
Simone Ferretti <simone.ferretti.work@gmail.com>
2024-10-03 20:49:32 +0200

    added pathos to dotfiles

---
+++ pathos.sh
@@ -0,0 +1,1 @@
+export PATH=/home/simone/.config/nvm/versions/node/v20.12.2/bin:/home/simone/.local/bin/go/bin:/home/simone/bin:/usr/local/bin:/home/simone/.local/bin:/usr/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/home/simone/.cargo/bin/:/home/simone/.proto/bin:/opt/lampp/bin/:/usr/bin/core_perl
